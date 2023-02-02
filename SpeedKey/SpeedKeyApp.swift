4e7854fc2a07b2920c3c90c706287cec3c147632
Nick Cook <nickcook@umich.edu>
2023-02-02 12:24:16 -0500

    Initial Commit

---
+++ SpeedKey/SpeedKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedKeyApp.swift
+//  SpeedKey
+//
+//  Created by Nick Cook on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeedKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
