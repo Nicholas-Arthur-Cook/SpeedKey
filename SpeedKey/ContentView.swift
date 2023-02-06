8a2b0dee30acacbcb3cec61c8d3e25dc19dd2cad
Rain Tarango <59982612+raintarango@users.noreply.github.com>
2023-02-06 18:40:29 -0500

    Created homepage

--- SpeedKey/ContentView.swift
+++ SpeedKey/ContentView.swift
@@ -9,12 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            VStack {
+                Text("SpeedKey")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.547, saturation: 0.571, brightness:0.819))
+                .padding([.bottom], 20)
+                
+                HStack {
+                    Text("Welcome to SpeedKey! The fast and accurate typing keyboard!")
+                        .multilineTextAlignment(.center)
+                        .font(.subheadline)
+                        .padding([.bottom], 60)
+                    
+                    
+                    
+                }
+                
+                VStack {
+        
+                    Button(
+                        "Enable SpeedKey",
+                        action: {
+                            //some code
+                            print("Sup")
+                        }
+                        )
+                    
+                    .padding([.bottom], 60)
+                    Button (
+                        "Practice Mode",
+                        action: {
+                            //some code
+                            print("Working on it")
+                        }
+                    )
+                    .padding([.bottom],60)
+                    Button(
+                        "Learn More about SpeedKey",
+                        action: {
+                            //code
+                            print("To-Do")
+                        })
+                    
+                    
+                }// VStack
+                        
+                    
+                    
+                
+               
+               
+            }
+
+      
+          
+            
         .padding()
     }
 }
