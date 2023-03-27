e05c198cc5795cdb85e1dd2419c2fd6a92b767e6
csnyders <csnyders@umich.edu>
2023-03-27 13:09:20 -0400

    Provide default for access when unset

--- SpeedKey Keyboard/AccessDefaults.swift
+++ SpeedKey Keyboard/AccessDefaults.swift
@@ -13,23 +13,37 @@ class AccessDefaults {
         return defaults?.object(forKey: key)
     }
     
-    public func getSpeedKeyOn() -> Bool { return getSetting(key: "speedKeyOn") as! Bool }
+    private let defaultShortcut = "-"
     
-    public func getAutoDeleteOn() -> Bool { return getSetting(key: "autoDeleteOn") as! Bool }
+    public func getSpeedKeyOn() -> Bool {return getSetting(key: "speedKeyOn") as? Bool ?? false}
     
-    public func getTypoNotificationOn() -> Bool { return getSetting(key: "typoNotificationOn") as! Bool }
+    public func getAutoDeleteOn() -> Bool {return getSetting(key: "autoDeleteOn") as? Bool ?? false}
     
-    public func getReviewPreivousWordsOn() -> Bool { return getSetting(key: "reviewPreivousWordsOn") as! Bool }
+    public func getTypoNotificationOn() -> Bool {return getSetting(key: "typoNotificationOn") as? Bool ?? false}
     
-    public func getNotificationType() -> String { return getSetting(key: "notificationType") as! String }
+    public func getReviewPreivousWordsOn() -> Bool {return getSetting(key: "reviewPreivousWordsOn") as? Bool ?? false}
     
-    public func getPreviousWordReviewCount() -> String { return getSetting(key: "previousWordReviewCount") as! String }
+    public func getNotificationType() -> String {
+        return getSetting(key: "notificationType") as? String ?? "Vibrate"
+    }
     
-    public func getEntireTextReviewShortcut() -> String { return getSetting(key: "entireTextReviewShortcut") as! String }
+    public func getPreviousWordReviewCount() -> String {
+        return getSetting(key: "previousWordReviewCount") as? String ?? "5"
+    }
     
-    public func getJumpToTypoShortcut() -> String { return getSetting(key: "jumpToTypoShortcut") as! String }
+    public func getEntireTextReviewShortcut() -> String {
+        return getSetting(key: "entireTextReviewShortcut") as? String ?? defaultShortcut
+    }
     
-    public func getWordDeletionShortcut() -> String { return getSetting(key: "wordDeletionShortcut") as! String }
+    public func getJumpToTypoShortcut() -> String {
+        return getSetting(key: "jumpToTypoShortcut") as? String ?? defaultShortcut
+    }
     
-    public func getCursorShortcut() -> String { return getSetting(key: "cursorShortcut") as! String }
+    public func getWordDeletionShortcut() -> String {
+        return getSetting(key: "wordDeletionShortcut") as? String ?? defaultShortcut
+    }
+    
+    public func getCursorShortcut() -> String {
+        return getSetting(key: "cursorShortcut") as? String ?? defaultShortcut
+    }
 }
